ed3597886b8f85a2134354047a5b0eeeabe34e40
hxuu <133578197+hxuu@users.noreply.github.com>
2026-03-02 23:35:57 +0100

    read from ring buffer

--- cmd/ataree/deps.go
+++ cmd/ataree/deps.go
@@ -2,8 +2,8 @@
 package ataree
 
 import (
-	_ github.com/spf13/cobra
-	_ github.com/spf13/pflag
+	_ "github.com/spf13/cobra"
+	_ "github.com/spf13/pflag"
 )
 
 // This file I only created to keep stuff in go.mod
