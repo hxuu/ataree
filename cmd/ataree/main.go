03db459afe96ab0f3d4d2d8503236f6e37973640
Achouak-Daamache <ra.daamache@esi-sba.dz>
2026-03-13 16:59:50 +0100

    fixed project structure

--- cmd/ataree/main.go
+++ cmd/ataree/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "github.com/hxuu/ataree/bpf"
+    "github.com/hxuu/ataree/internal/ebpf"
 )
 
 func main() {
     println("runtimed starting...")
     println("loading eBPF objects...")
 
-    var objs bpf.MinimaltraceObjects 
-    if err := bpf.LoadMinimaltraceObjects(&objs, nil); err != nil {
+    var objs ebpf.MinimaltraceObjects 
+    if err := ebpf.LoadMinimaltraceObjects(&objs, nil); err != nil {
         println("error loading eBPF objects: ", err)
     }
     defer objs.Close()
